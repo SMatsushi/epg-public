ca48611fdb0a42da38993109edc9c53253cfa12a
Apache <matsushi2009@gmail.com>
2015-01-07 17:16:25 +0900

    now saving ts file

--- do-record.sh
+++ do-record.sh
@@ -10,6 +10,7 @@ echo "SID  : $SID"
 RECORDER=/usr/local/bin/recpt1
 PERL=/usr/local/bin/perl
 ENCPROG=/home/www/epgrec/tsencode.pl
+ENC2PROG=/home/www/epgrec/tsencode2.pl
 
 if [ ${MODE} = 0 ]; then
    # MODE=0では必ず無加工のTSを吐き出すこと
@@ -21,7 +22,9 @@ elif [ ${MODE} == 2 ]; then
     $RECORDER --b25 --strip $CHANNEL $DURATION ${OUTPUT}_tmp.ts >/dev/null
     # <-解像度は4:3で指定（理由は後述）
     $PERL $ENCPROG ${OUTPUT}_tmp.ts ${OUTPUT} 640x360
-    rm ${OUTPUT}_tmp.ts
+#    $PERL $ENC2PROG ${OUTPUT}_tmp.ts t3.mp4 640x360
+#    rm ${OUTPUT}_tmp.ts
+
 # mode 2 example is as follows
 #elif [ ${MODE} = 2 ]; then
 #   $RECORDER $CHANNEL $DURATION ${OUTPUT}.tmp.ts --b25 --strip
